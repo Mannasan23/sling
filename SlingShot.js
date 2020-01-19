ba0831f952dced5e5315c82c2f463f929ca0b7af
Manna Sandhu <59117419+Mannasan23@users.noreply.github.com>
2020-01-19 13:03:01 +0530

    Update SlingShot.js

--- SlingShot.js
+++ SlingShot.js
@@ -12,11 +12,24 @@ class SlingShot{
     }
 
     display(){
-        if(this.sling.bodyA){
+       if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push();
+            stroke(48,22,8);
+            if(pointA.x<220){
+                strokeWeight(7);
+                line(pointA.x-25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-25, pointA.y, pointB.x+30, pointB.y-3);
+                image(this.sling3, pointA.x-30, pointA.y-10, 15, 30);
+            }
+        else{
+                strokeWeight(4);
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+                image(this.sling3, pointA.x+30, pointA.y-10, 15, 30);
+            }
+            pop();
         }
     }
     fly(){
